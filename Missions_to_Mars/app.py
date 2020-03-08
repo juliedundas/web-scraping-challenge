4ac87a08e543c82308673f2b50eb1576770a2723
juliedundas <juliedundas@Julies-MacBook-Pro-2.local>
2020-03-07 17:18:17 -0700

    flask app almost complete

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -2,18 +2,38 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
+# Create an instance of Flask
 app = Flask(__name__)
 
-mongo = PyMongo(app)
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars")
 
+
+# Route to render index.html template using data from Mongo
 @app.route("/")
-def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars = mars)
+def home():
 
+    # Find one record of data from the mongo database
+    mars = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars=mars)
+
+
+# Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars 
+    mars = mongo.db.mars
+
+    # Run the scrape function
     mars_data = scrape_mars.scrape()
+
+    # Update the Mongo database using update and upsert=True
     mars.update({}, mars_data, upsert=True)
-    return redirect("http://localhost:5000/", code=302)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -3,26 +3,22 @@ from splinter import Browser
 from bs4 import BeautifulSoup
 #pip install ipython
 #pip install nbconver
+import requests
+import pandas as pd
 
 
 #Site Navigation
-executable_path = {"executable_path": "/Users/sharonsu/Downloads/chromedriver"}
-browser = Browser("chrome", **executable_path, headless=False)
+def init_browser():
+    executable_path = {"executable_path": "/Users/sharonsu/Downloads/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
 
-
-# Defining scrape & dictionary
-def scrape():
-    final_data = {}
-    output = marsNews()
-    final_data["mars_news"] = mars_new()
-    final_data["mars_image"] = mars_image()
-    final_data["mars_facts"] = mars_facts()
-    final_data["mars_hemisphere"] = mars_hem()
-
-    return final_data
 
 #Mars News scrape function
-def mars_news():
+def scrape():
+
+    #dictionary to hold all scraped data:
+    mars_data = {}
+
     # URL of page to be scraped
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     # Retrieve page with the requests module
@@ -35,32 +31,31 @@ def mars_news():
     article_list = []
     for result in results:
     # Error handling
-        try:
         # Identify and return title of article
-            time.sleep(3)
-            news_title = result.find('div', class_="content_title")
-            title = news_title.find('a').text
-            # Identify and return description of article
-            news_p = result.find('div', class_="rollover_description")
-            description = news_p.find('div', class_="rollover_description_inner").text
+         #time.sleep(3)
+        news_title = result.find('div', class_="content_title")
+        title = news_title.find('a').text
+        # Identify and return description of article
+        news_p = result.find('div', class_="rollover_description")
+        description = news_p.find('div', class_="rollover_description_inner").text
         
-        # Print results only if title, price, and link are available
-            if (news_title and news_p):
-                print('-------------')
-                print(f'Article Title: {title}')
-                article_list.append(result)
-                print(f'Article Description: {description}')
-                #description_list.append(result)
+        # Print results only if title, and link are available
+        if (news_title and news_p):
+            print('-------------')
+            print(f'Article Title: {title}')
+            article_list.append(result)
+            print(f'Article Description: {description}')
+            #description_list.append(result)
            
-        except AttributeError as e:
-            print(e)
-    #Print the latest news title and description
-    print(f'Latest News Title: {title}')
-    print(f'Description of Latest News Title: {description}')
+    
+        #Print the latest news title and description
+        print(f'Latest News Title: {title}')
+        print(f'Description of Latest News Title: {description}')
 
+    mars_data['news_title'] = title
+    mars_data['news_desc'] = description
 
 #Mars image scrape function Space Images - Featured Image
-def mars_image():
     executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
     browser = Browser('chrome', **executable_path, headless=False)
     url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
@@ -78,12 +73,13 @@ def mars_image():
     featured_image_url = main_url + image_url
     print(f"Click this link to see the current featured image: {featured_image_url}")
 
+    mars_data['feature_image'] = featured_image_url
+
 #Mars  Weather - not required
 
 
 
 # Mars Facts scrape function
-def mars_facts():
     #Create link for pandas URL
     pandas_url = 'https://space-facts.com/mars/'
     tables = pd.read_html(pandas_url)
@@ -96,6 +92,86 @@ def mars_facts():
     df.to_html('table.html')    
 
 # Mars hemisphere scrape:
-def mars_hem():  
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    url_start = 'https://astrogeology.usgs.gov'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    #Create a loop to get the link to each page that will be looped through to collect title and image url  
 
-    print(final_data)
+    img_urls = []
+
+    hems = soup.find_all('div', class_="item")
+
+    for item in hems:
+        link = item.find('a')
+        href = link['href']
+        url = url_start + href
+        print(url)
+        
+        img_urls.append(url)
+        
+        #print(img_urls)      
+
+       # print(final_data)
+
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # find all div classes 'item'
+    hemispheres = soup.find_all('div', class_="item")
+
+    # get list of URLs for each hemisphere
+    img_url_text = []
+
+    for item in hemispheres:
+        # Use Beautiful Soup's find() method to navigate and retrieve attributes
+        
+        link = item.find('a')
+        href = link['href']
+       
+        #print('-----------')
+        #print(header.text)
+       
+        url = ('https://astrogeology.usgs.gov' + href)
+        #print(url)
+        
+        #dict = {'title':header.text}
+        #titles.append(dict)
+        img_url_text.append(url)
+
+        hemisphere_image_urls = []
+#title_list = []
+
+    for url in img_url_text:
+        #time.sleep(2)
+        browser.visit(url)
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+        
+        titles = soup.find('h2',class_="title")
+        browser.links.find_by_text('Sample')
+        image = browser.windows[0].next.url
+        
+        urls = {
+            'title':titles.text,
+            'img_url':image
+        }
+
+          
+        #title_list.append(titles)
+        hemisphere_image_urls.append(urls)
+        
+        print(titles.text)
+        print(image)
+        print('-----------')
+
+        print(hemisphere_image_urls)
+
+    mars_data['images'] = urls
+
+    browser.quit()
+
+    return mars_data
